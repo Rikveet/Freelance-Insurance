6174d2a505ece8749ec701d46008a187eb36bf60
CaptKillSwitch <rh18vo@gmail.com>
2023-03-31 15:11:15 -0400

    Instagram images not loading fix

--- next.config.js
+++ next.config.js
@@ -5,20 +5,6 @@ const nextConfig = {
     appDir: true,
   },
   images: {
-      remotePatterns: [
-          {
-              protocol: 'https',
-              hostname: 'scontent.cdninstagram.com',
-              port: '',
-              pathname: '**',
-          },
-          {
-              protocol: 'https',
-              hostname: 'video.cdninstagram.com',
-              port: '',
-              pathname: '**',
-          },
-      ],
       domains: ["scontent.cdninstagram.com", "video.cdninstagram.com"]
   },
 }
