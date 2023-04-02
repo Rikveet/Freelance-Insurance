db1ce9b2109c354e4697f0d16a1a1b69ed2680bb
CaptKillSwitch <rh18vo@gmail.com>
2023-04-02 13:25:56 -0400

    Fix for form title not working on ios

--- src/app/components/Forms/Title/index.tsx
+++ src/app/components/Forms/Title/index.tsx
@@ -9,17 +9,16 @@ const BlueWave = () => (
     </svg>
 )
 const GreyWave = () => (
+    <>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <g>
+                <path fill="#16296A" fillOpacity="1"
+                      d="M0,256L80,261.3C160,267,320,277,480,240C640,203,800,117,960,96C1120,75,1280,117,1360,138.7L1440,160L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path>
+            </g>
+        </svg>
+        <TypeWriter className={styles.Title} constantText={'get'} texts={['quote', 'advice', 'insurance']}/>
+    </>
 
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-        <g>
-            <path fill="#16296A" fillOpacity="1"
-                  d="M0,256L80,261.3C160,267,320,277,480,240C640,203,800,117,960,96C1120,75,1280,117,1360,138.7L1440,160L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path>
-            <foreignObject className={styles.TitleContainer}>
-                <TypeWriter className={styles.Title} constantText={'get'} texts={['quote', 'advice', 'insurance']}/>
-            </foreignObject>
-        </g>
-
-    </svg>
 )
 
 const Title = () => (
