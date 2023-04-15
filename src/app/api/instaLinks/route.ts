fba6afb802243f0479f8b87c2a8423457fffe7bb
CaptKillSwitch <rh18vo@gmail.com>
2023-04-15 15:46:42 -0400

    Fix caching next js api request, causing instagram component to fail.

--- src/app/api/instaLinks/route.ts
+++ src/app/api/instaLinks/route.ts
@@ -3,9 +3,9 @@ import {NextResponse} from "next/server";
 export async function GET() {
     const instaAccessToken = process.env.INSTA_TOKEN;
     const result = await fetch(`https://graph.instagram.com/me/media?fields=id,media_type,media_url,thumbnail_url,username,timestamp,caption,permalink&access_token=${instaAccessToken}`
-        , {cache: 'no-store'})
+        , {cache: 'no-cache'})
         .then(async res => {
-            return (res.status)
+            return (await res.json()).data
         }).catch(e => e.toString())
     return NextResponse.json({data: result})
 }
