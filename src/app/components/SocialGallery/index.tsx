2d67e82c03b4a5703f6c2b4b9cb8331db7484427
CaptKillSwitch <rh18vo@gmail.com>
2023-04-19 14:41:32 -0400

    Removed api link for getting instagram media links. Used ssr functionality of next.js 13 to handle getting media links. Removed use client from root component to enable ssr for routes.

--- src/app/components/SocialGallery/index.tsx
+++ src/app/components/SocialGallery/index.tsx
@@ -3,48 +3,54 @@ import styles from './index.module.css';
 
 async function getMediaInfo() {
     const instaAccessToken = process.env.INSTA_TOKEN;
-    const results = await fetch(`https://graph.instagram.com/me/media?fields=id,media_type,media_url,thumbnail_url,username,timestamp,caption,permalink&access_token=${instaAccessToken}`
-        , {cache: 'no-store'})
+    const res: {
+        status: number,
+        data: Link[]
+    } = await fetch(`https://graph.instagram.com/me/media?fields=id,media_type,media_url,thumbnail_url,username,timestamp,caption,permalink&access_token=${instaAccessToken}`
+        , { next: { revalidate: 60 } })
         .then(async res => {
-            return (await res.json()).data as Link[]
-        }).catch(_ => {
-            return undefined
-        })
-    if (!results) {
-        return []
-    }
-    return results.map(link => {
-        const date = new Date(link.timestamp)
-        const monthNames = ["January", "February", "March", "April", "May", "June",
-            "July", "August", "September", "October", "November", "December"
-        ];
-        const nth = function (d: number) {
-            if (d > 3 && d < 21) return 'th';
-            switch (d % 10) {
-                case 1:
-                    return "st";
-                case 2:
-                    return "nd";
-                case 3:
-                    return "rd";
-                default:
-                    return "th";
+            if (res.status === 200) {
+                return {status: res.status, data: (await res.json()).data as Link[]}
             }
-        }
-        return {
-            ...link,
-            timestamp: `${date.getDate()}${nth(date.getDate())} ${monthNames[date.getMonth()]} ${date.getFullYear() % 100}`
-        }
-    })
+            return {status: res.status, data: [] as Link[]}
+        }).catch(_ => {
+            return {status: 403, data: [] as Link[]}
+        })
+    return {
+        ...res,
+        data: res.data.map(link => {
+            const date = new Date(link.timestamp)
+            const monthNames = ["January", "February", "March", "April", "May", "June",
+                "July", "August", "September", "October", "November", "December"
+            ];
+            const nth = function (d: number) {
+                if (d > 3 && d < 21) return 'th';
+                switch (d % 10) {
+                    case 1:
+                        return "st";
+                    case 2:
+                        return "nd";
+                    case 3:
+                        return "rd";
+                    default:
+                        return "th";
+                }
+            }
+            return {
+                ...link,
+                timestamp: `${date.getDate()}${nth(date.getDate())} ${monthNames[date.getMonth()]} ${date.getFullYear() % 100}`
+            }
+        })
+    }
 }
 
 
 const SocialGallery = async () => {
-    const links = await getMediaInfo()
+    const res = await getMediaInfo()
     return (
-        links && links?.length > 0 ?
+        res.status === 200 && res.data.length > 0 ?
             <div className={styles.Container} id={'media'}>
-                <InstagramCarousel postLinks={links}/>
+                <InstagramCarousel postLinks={res.data}/>
             </div> :
             <></>
     )
